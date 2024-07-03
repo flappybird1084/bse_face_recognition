f5829ff67daafc08e7987b5ca976200063894c7c
rianbutala <rianbutala@gmail.com>
2024-07-03 16:03:43 -0700

    small update with directories in sh file

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-pyenv shell 3.12.4
+#pyenv shell 3.12.4
+cd /home/rianbutala/Desktop/face-recognition-pi2/bse_face_recognition/
 nohup python3 camstream_model_both.py --headless &
 cd /home/rianbutala/Desktop/face-recognition-pi2/bse_face_recognition/streamlit/ 
 nohup streamlit run Main_Page.py & 
