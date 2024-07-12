611f92472390ea83f091e1f6b3bb028dbe6f0064
rianbutala <rianbutala@gmail.com>
2024-07-12 14:55:40 -0700

    added image recycling (due to nc being weird it automatically resets the image once sent). now we're changing the name

--- message_monitoring.py
+++ message_monitoring.py
@@ -1,6 +1,7 @@
 from message_controller import monitor_for_message, monitor_for_image
+import subprocess
 
 while True:
     #monitor_for_message("1234")
     monitor_for_image("1234")
-
+    process = subprocess.Popen(["mv streamlit_detection_image_2.jpg streamlit_detection_image.jpg"])
