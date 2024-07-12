5bb3bb5253c90405f1f6638c84bf4fa2984c1359
rianbutala <rianbutala@gmail.com>
2024-07-12 15:05:19 -0700

    better file handling

--- message_monitoring.py
+++ message_monitoring.py
@@ -4,4 +4,4 @@ import subprocess
 while True:
     #monitor_for_message("1234")
     monitor_for_image("1234")
-    process = subprocess.Popen("mv streamlit_detection_image_2.jpg streamlit_detection_image.jpg", shell = True)
+    process = subprocess.Popen("mv temp/streamlit_detection_image_2.jpg temp/streamlit_detection_image.jpg", shell = True)
