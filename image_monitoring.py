fb56a9a1b1a41235bc81c3c778833c14e93ae935
rianbutala <rianbutala@gmail.com>
2024-07-15 13:55:28 -0700

    refined image showing thread. apparently ui interactions all happen on the main thread

--- image_monitoring.py
+++ image_monitoring.py
@@ -1,21 +1,38 @@
 from message_controller import monitor_for_image
 import cv2
 from threading import Thread
+import time
 
 def display_loop():
+    print("display thread started")
     while True:
-        image = cv2.imread("temp/streamlit_detection_image.jpg")
-        cv2.imshow("frame", image)
+        print("about to show image")
+        try:
+            image = cv2.imread("temp/streamlit_detection_image.jpg")
+            cv2.imshow("frame", image)
+            print("image shown")
+            cv2.waitKey(1)
+        except Exception as e:
+            print("image not shown")
+            print (e)
+        
+        time.sleep(0.25)
+
 
 def monitor_loop():
+    print("monitor thread started")
     while True:
+        print("about to monitor for image")
         monitor_for_image("1234")
+        print("image found")
+        time.sleep(0.25)
 
 
 display_thread = Thread(target=display_loop)
 monitor_thread = Thread(target=monitor_loop)
 
+
+monitor_thread.start()
 display_thread.run()
-monitor_thread.run()
 
 
