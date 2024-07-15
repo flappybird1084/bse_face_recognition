64861cde6c33750e06fe220e91ff40548a717859
rianbutala <rianbutala@gmail.com>
2024-07-15 13:33:45 -0700

    tweaks to image monitoring

--- image_monitoring.py
+++ image_monitoring.py
@@ -2,5 +2,6 @@ from message_controller import monitor_for_image
 import cv2
 
 while True:
+    image = cv2.imread("temp/streamlit_detection_image.jpg")
+    cv2.imshow("frame", image)
     monitor_for_image("1234")
-    cv2.imshow("frame", "temp/streamlit_detection_image.jpg")
